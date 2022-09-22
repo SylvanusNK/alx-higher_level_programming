e21ccbb289206c9b721cdd5f46d7d16c83ff6dfb
SylvanusNK <nkamabasil@gmail.com>
2022-09-22 16:07:15 -0700

    hidden things

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,6 @@
-#!/urs/bin/python3
+#!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
     for s in dir(hidden_4):
         if s[:2] != "__":
-             print("{:s}".format(s))
+            print("{:s}".format(s))
